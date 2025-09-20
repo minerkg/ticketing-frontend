0d9f40aac63b85bba7acc182f75ae32bc77cd5d3
minerkg <129666850+minerkg@users.noreply.github.com>
2025-09-20 14:48:32 +0300

    frontend style facelift

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import {Card} from 'primeng/card';
+import {Button} from 'primeng/button';
+import {RouterLink} from '@angular/router';
+import {PrimeTemplate} from 'primeng/api';
+import {Image} from 'primeng/image';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Card,
+    Button,
+    RouterLink,
+    PrimeTemplate,
+    Image
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
